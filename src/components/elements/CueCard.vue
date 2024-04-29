52fa3a2ff106205b68050235b64f275e30c1b0b7
Cait <2423179@dundee.ac.uk>
2024-04-29 14:18:38 +0100

    - Basic nav-bar & styling - basic Navigation - colour scheme - comments

---
+++ src/components/elements/CueCard.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="card">
+    <img alt="Cue card Cover Art" src="https://picsum.photos/200/200"/>
+    <div class="card-body">
+      <h2>Title Deck</h2>
+      <div class="gridContainer">
+        <!--  Column Left -->
+        <div class="gridItem">
+          <p class="tag" v-for="tag in tags">{{ tag }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  @import '../../scss/elements/_cuecard';
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        tags: ["cait", "loves", "adam", "very", "much"]
+      };
+    }
+  };
+</script>
+<!--would need
+- name of deck
+- image of deck
+- tags for deck
+- words of deck
+- id for each deck
+
+//-------
+table 1 : deck identify
+- uuid
+- display_name
+- display_image
+- tags
+
+table 2 : deck contents
+list of records containing each word for the deck
+- uuid
+- word
+- word 2
+- type
+-->
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,14 +1,73 @@
 <script setup>
-
+  import CueCard from '../elements/CueCard.vue'
 </script>
 
 <template>
-  <div class="main-container">
-    <p>Hi this is home</p>
+<!--  Main Parent Container-->
+  <div class="mainContainer">
+<!--    Container for the Columns-->
+    <div class="gridContainer">
+      <!--  Column Left -->
+      <div class="gridItem">
+        <div  class="deckContainer">
+          <div>
+            <h3>Name of Deck</h3>
+            <h3> Tags</h3>
+            <h3> Logo</h3>
+          </div>
+          <div v-for="card in Cards">
+            <p>{{ card.name_of_deck }}</p>
+            <p> {{ card.tags }}</p>
+            <span class="material-symbols-outlined"> {{ card.logo}}</span>
+          </div>
+
+        </div>
+        <div>
+          <button class="addCard" type="button">Add Card</button>
+          <button class="deleteCard" type="button">Delete Card</button>
+        </div>
+      </div>
+      <!-- Column Right -->
+      <div class="gridItem">
+        <CueCard></CueCard>
+      </div>
+    </div>
 
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      Cards: [
+        {
+          name_of_deck: "French",
+          tags: "bonjour, salut, coucou",
+          logo: "flag",
+        },
+
+        {
+          name_of_deck: "English",
+          tags: "hello, hi, hey",
+          logo: "sports_score"
+        },
+
+        {
+          name_of_deck: "Baking",
+          tags: "flambe, brulee, yum",
+          logo: "cookie"
+        },
+
+        {
+          name_of_deck: "Je ne sais pas",
+          tags: "avoir, savoir, manger",
+          logo: "edit"
+        },
+    ]};
+  }
+};
+</script>
 <style scoped lang="scss">
   @import '../../scss/pages/_home';
 </style>
